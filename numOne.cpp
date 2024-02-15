c1d109a2376d56bb3f232068f5541c7703543f21
Jan Carlo Paredes <paredesjancarlo99@gmail.com>
2024-02-15 15:04:16 +0800

    initial commit

---
+++ numOne.cpp
@@ -0,0 +1,12 @@
+//Create a simple calculator program for a cashier in a grocery. Your program will require to input the price of three items, the payment of the customer; and also if the customer is a senior citizen or not. Use the following below as a reference for your computation.
+//    •	All items are subject to 10% tax rate.
+//    •	If the customer is a senior citizen, the have an discount of 20%
+//    •	At the end of the program, display the change of the customer. Display also if the payment of the customer is insufficient.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
